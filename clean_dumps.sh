2f4e859689697dfe3b233ff5ce187f92a707d4fb
OleksiyKhudoshyn <hudoshin7605@gmail.com>
2024-12-17 18:49:45 +0100

    Complete import fresh dumps from server2 on server1 2.1 (bash-version)

--- clean_dumps.sh
+++ clean_dumps.sh
@@ -5,11 +5,11 @@ SERVER1_USER="MATS-VM-01_admin"
 SERVER1_IP="172.201.121.48"
 SSH_KEY="/root/.ssh/Olex.pem"
 REMOTE_DUMP_PATH="/var/lib/mongodb/download/dump"
+FETCH_SCRIPT_PATH="/home/MATS-VM-01_admin/fetch_latest_dump.sh"
 
 echo "Deleting old dumps on server 1..."
 
 # Connect to server 1 and remove old dumps
-# shellcheck disable=SC2087
 ssh -i "$SSH_KEY" "${SERVER1_USER}@${SERVER1_IP}" <<EOF
 echo "Connected to server 1. Removing old dumps..."
 if ! sudo rm -rf ${REMOTE_DUMP_PATH}/*; then
@@ -17,4 +17,12 @@ if ! sudo rm -rf ${REMOTE_DUMP_PATH}/*; then
   exit 1
 fi
 echo "Old dumps successfully deleted."
+
+# Execute fetch_latest_dump.sh script
+echo "Executing fetch_latest_dump.sh..."
+if ! sudo bash ${FETCH_SCRIPT_PATH}; then
+  echo "Error occurred while executing fetch_latest_dump.sh."
+  exit 1
+fi
+echo "Successfully fetched the latest dumps."
 EOF
