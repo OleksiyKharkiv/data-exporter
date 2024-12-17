e356d802d17c2a2281d57fbbcd55459d1faa174a
OleksiyKhudoshyn <hudoshin7605@gmail.com>
2024-12-17 18:50:06 +0100

    Complete import fresh dumps from server2 on server1 2.1 (bash-version)

--- clean_dumps.sh
+++ clean_dumps.sh
@@ -10,6 +10,7 @@ FETCH_SCRIPT_PATH="/home/MATS-VM-01_admin/fetch_latest_dump.sh"
 echo "Deleting old dumps on server 1..."
 
 # Connect to server 1 and remove old dumps
+# shellcheck disable=SC2087
 ssh -i "$SSH_KEY" "${SERVER1_USER}@${SERVER1_IP}" <<EOF
 echo "Connected to server 1. Removing old dumps..."
 if ! sudo rm -rf ${REMOTE_DUMP_PATH}/*; then
