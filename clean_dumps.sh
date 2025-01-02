20f2871771004855c74fc1569793802d0468dfed
OleksiyKhudoshyn <hudoshin7605@gmail.com>
2025-01-02 13:50:57 +0100

    Complete import fresh dumps from server2 on server1 & restore dumps to MongoDB DBs, copying collections inside DBs 3.1 (bash-version)

--- clean_dumps.sh
+++ clean_dumps.sh
@@ -29,13 +29,67 @@ echo "Successfully fetched the latest dumps."
 
 # Drop specified databases
 echo "Dropping old databases..."
-mongo --eval 'db.getSiblingDB("mats-diagnostic").dropDatabase()'
-mongo --eval 'db.getSiblingDB("mats-payment").dropDatabase()'
-mongo --eval 'db.getSiblingDB("mats-training-plan").dropDatabase()'
-mongo --eval 'db.getSiblingDB("mats-user").dropDatabase()'
+mongosh --eval 'db.getSiblingDB("mats-diagnostic").dropDatabase()'
+mongosh --eval 'db.getSiblingDB("mats-payment").dropDatabase()'
+mongosh --eval 'db.getSiblingDB("mats-training-plan").dropDatabase()'
+mongosh --eval 'db.getSiblingDB("mats-user").dropDatabase()'
 
 # Restore latest dump
 echo "Restoring the latest dump..."
-mongorestore --gzip /var/lib/mongodb/download/dump
+timeout 10m mongorestore --gzip /var/lib/mongodb/download/dump
+
+# MongoDB processing scripts after restore
+echo "Running MongoDB processing scripts..."
+
+# Script 1: Copy data between mats-training-plan and mats-payment
+timeout 10m mongosh --eval '
+const srcDB = db.getSiblingDB("mats-training-plan");
+const destDB = db.getSiblingDB("mats-payment");
+
+// Assuming you want to copy specific collections, for example, "trainingPlans"
+const documents = srcDB.trainingPlan.find().toArray();
+if (documents.length > 0) {
+    destDB.tempTrainingPlan.insertMany(documents);
+}
+'
+
+# Script 2: Copy stripePayment collection from mats-payment to mats-user
+timeout 10m mongosh --eval '
+const srcDB = db.getSiblingDB("mats-payment");
+const destDB = db.getSiblingDB("mats-user");
+
+// Get all documents from the stripePayment collection
+const documents = srcDB.stripePayment.find().toArray();
+if (documents.length > 0) {
+    destDB.tempStripePayment.insertMany(documents);
+}
+'
+
+# Script 3: Copy userSubscription collection from mats-payment to mats-user
+timeout 10m mongosh --eval '
+const srcDB = db.getSiblingDB("mats-payment");
+const destDB = db.getSiblingDB("mats-user");
+
+// Get all documents from the userSubscription collection
+const documents = srcDB.userSubscription.find().toArray();
+if (documents.length > 0) {
+    destDB.tempUserSubscription.insertMany(documents);
+}
+'
+
+# Script 4: Copy consumableProduct collection from mats-payment to mats-user
+timeout 10m mongosh --eval '
+const srcDB = db.getSiblingDB("mats-payment");
+const destDB = db.getSiblingDB("mats-user");
+
+// Get all documents from the consumableProduct collection
+const documents = srcDB.consumableProduct.find().toArray();
+if (documents.length > 0) {
+    destDB.tempConsumableProduct.insertMany(documents);
+}
+'
+echo "Script executed successfully."
 
 EOF
+
+echo "Script executed successfully."
